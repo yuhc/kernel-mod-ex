74df9c3c523cf4d29cd7c0a62209e8d152fd53ad
Hangchen Yu <hyu@cs.utexas.edu>
2017-04-20 10:52:05 -0500

    Procfs supports write

---
+++ proc_filesystem/procfs-2.c
@@ -0,0 +1,74 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/proc_fs.h>
+#include <asm/uaccess.h>
+
+#define min_v(a,b) (a>b?b:a)
+
+#define PROCFS_NAME     "hello_world"
+#define PROCFS_MAX_SIZE 1024
+
+ssize_t procfs_read(struct file *, char __user *, size_t, loff_t *);
+ssize_t procfs_write(struct file *, const char __user *, size_t, loff_t *);
+
+struct proc_dir_entry *Proc_File;
+static char procfs_buffer[PROCFS_MAX_SIZE];
+static size_t procfs_buffer_size = 0;
+
+const struct file_operations proc_fops = {
+    .owner = THIS_MODULE,
+    .read = procfs_read,
+    .write = procfs_write
+};
+
+ssize_t procfs_read(struct file *filp, char __user *buffer, size_t length,
+        loff_t *offset) {
+    int ret;
+
+    printk(KERN_INFO "procfs_read (/proc/%s) called\n", PROCFS_NAME);
+    if (*offset > 0) {
+        /* read has been finished */
+        ret = 0;
+    }
+    else {
+        /* fill the buffer */
+        ret = min_v(length, procfs_buffer_size);
+        copy_to_user(buffer, procfs_buffer, ret);
+//      ret = 0;
+//      msg_ptr = msg;
+//      while (*msg_ptr && ret < length) {
+//          put_user(*(msg_ptr++), buffer++);
+//          ret++;
+//      }
+    }
+
+    *offset += ret;
+    return ret;
+}
+
+ssize_t procfs_write(struct file *filp, const char __user *buffer,
+        size_t length, loff_t *offset) {
+    procfs_buffer_size = min_v(length, PROCFS_MAX_SIZE);
+    if (copy_from_user(procfs_buffer, buffer, procfs_buffer_size))
+        return -EFAULT;
+
+    return procfs_buffer_size;
+}
+
+int init_module(void) {
+    Proc_File = proc_create(PROCFS_NAME, 0644, NULL, &proc_fops);
+    if (Proc_File == NULL) {
+        return -ENOMEM;
+    }
+    sprintf(procfs_buffer, "Hello World!\n");
+    procfs_buffer_size = strlen(procfs_buffer) + 1;
+
+    printk(KERN_INFO "/proc/%s created\n", PROCFS_NAME);
+    return 0;
+}
+
+void cleanup_module(void) {
+    // remove_proc_entry(procfs_name, Proc_File);
+    proc_remove(Proc_File);
+    printk(KERN_INFO "/proc/%s removed\n", PROCFS_NAME);
+}
